dc614d2c207d7c327c1c7382f5e30a9c0298d6cb
Alex Tercete <alextercete@gmail.com>
2016-10-17 16:25:46 +0100

    Create basic classes

--- src/DependencyInjection.Console/Program.cs
+++ src/DependencyInjection.Console/Program.cs
@@ -10,6 +10,47 @@ namespace DependencyInjection.Console
     {
         static void Main(string[] args)
         {
+
+        }
+
+        class MazeSimulation
+        {
+            private IMazeWriter _mazeWriter;
+            private IMazeGenerator _mazeGenerator;
+
+            public MazeSimulation()
+            {
+                _mazeWriter = new ConsoleMazeWriter();
+                _mazeGenerator = new MazeGenerator();
+            }
+        }
+
+        private interface IMazeWriter
+        {
+            void Write(Maze maze);
+        }
+
+        class ConsoleMazeWriter : IMazeWriter
+        {
+            public void Write(Maze maze)
+            {
+            }
+        }
+
+        private interface IMazeGenerator
+        {
+        }
+
+        class MazeGenerator : IMazeGenerator
+        {
+            public Maze Generate()
+            {
+                return new Maze();
+            }
+        }
+
+        class Maze
+        {
         }
     }
 }
