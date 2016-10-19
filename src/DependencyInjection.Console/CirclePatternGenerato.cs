d49be945d0d569c02f5772052f8f5583f1d56037
Michael Clark <Michael.Clark@red-gate.com>
2016-10-19 10:39:52 +0100

    Add circle pattern generator

---
+++ src/DependencyInjection.Console/CirclePatternGenerato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DependencyInjection.Console
+{
+    internal class CirclePatternGenerator : IPatternGenerator
+    {
+        public Pattern Generate(int width, int height)
+        {
+            var generate = new Pattern(width, height);
+            var squares = generate.Squares;
+
+            for (var i = 0; i < width; ++i)
+            {
+                for (var j = 0; j < height; ++j)
+                {
+                    squares[i, j] = Math.Pow(Math.Pow(i/ (double)(width-1) - 0.5,2) + Math.Pow(j/(double)(height-1) - 0.5, 2), 0.5) < 0.5 ? Square.White : Square.Black;
+                }
+            }
+
+            return generate;
+        }
+    }
+}
