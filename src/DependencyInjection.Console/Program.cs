7e5bcf162c559930c758236315b0acd178ab8630
Alex Tercete <alextercete@gmail.com>
2016-10-25 17:08:03 +0100

    Parse (but don't use) `pattern` command-line arg

--- src/DependencyInjection.Console/Program.cs
+++ src/DependencyInjection.Console/Program.cs
@@ -9,12 +9,14 @@ namespace DependencyInjection.Console
             var useColors = false;
             var width = 25;
             var height = 15;
+            var pattern = "circle"; // TODO: Hook this up
 
             var optionSet = new OptionSet
             {
                 {"c|colors", value => useColors = value != null},
                 {"w|width=", value => width = int.Parse(value)},
-                {"h|height=", value => height = int.Parse(value)}
+                {"h|height=", value => height = int.Parse(value)},
+                {"p|pattern=", value => pattern = value}
             };
             optionSet.Parse(args);
 
